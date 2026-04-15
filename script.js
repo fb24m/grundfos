b57accab5598cd073e16fac5b666a85fcadd04c2
fb24m <hello@fb24m.ru>
2026-04-15 15:48:16 +0400

    finish product-details

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
-
 	const previewsSlider = new Swiper('.previews-slider', {
 		slidesPerView: 5,
 		spaceBetween: 8,
@@ -11,5 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
 		thumbs: {
 			swiper: previewsSlider,
 		},
+	})
+
+	const unfoldButtons = document.querySelectorAll('[data-unfold]')
+
+	unfoldButtons.forEach(button => {
+		const el = document.querySelector(button.dataset.unfold)
+		const foldedText = button.querySelector('.folded')
+		const unfoldedText = button.querySelector('.unfolded')
+
+		if (el) {
+			button.addEventListener('click', () => {
+				el.classList.toggle('unfold')
+
+				if (foldedText && unfoldedText) {
+					if (el.classList.contains('unfold')) {
+						foldedText.style.display = 'none'
+						unfoldedText.style.display = 'block'
+					}
+					else {
+						foldedText.style.display = 'block'
+						unfoldedText.style.display = 'none'
+					}
+				}
+			})
+		}
 	})
 })
